8118b4a3ea778a7b3ef97ccbc26be09b9472d0b0
Yitzhak Bar Geva <yitzhakbargeva@gmail.com>
2015-12-27 09:59:15 -0500

    Wasn't being tracked for some reason

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+WATCH="${HUGO_WATCH:=false}"
+SLEEP="${HUGO_REFRESH_TIME:=-1}"
+echo "HUGO_WATCH:" $WATCH
+echo "HUGO_REFRESH_TIME:" $HUGO_REFRESH_TIME
+echo "HUGO_THEME:" $HUGO_THEME
+echo "HUGO_BASEURL" $HUGO_BASEURL
+echo "HUGO_DEST" $HUGO_DEST
+
+HUGO=/usr/bin/hugo
+
+while [ true ]
+do
+    find {e,h}content static | entr ./fmks $HUGO_DEST $HUGO_BASEURL
+    echo "Sleeping for $HUGO_REFRESH_TIME seconds..."
+    sleep $SLEEP
+done
+
