30e4eaeb2bc78231edd03435a570085293a6005b
yitzhakbg <yitzhakbargeva@gmail.com>
2016-01-11 19:30:56 +0200

    Documented why it doesn't work in Docker on Mac

--- docker-hugo/run.sh
+++ docker-hugo/run.sh
@@ -10,9 +10,14 @@ echo "HUGO_DEST" $HUGO_DEST
 
 HUGO=/usr/bin/hugo
 
-while [ true ]
-do
-    find config{e,h}.toml {e,h}content static | entr ./fmks $HUGO_DEST $HUGO_BASEURL
+# Note: This doesn't work on Mac. Apparently entr monitors the file system in the VM in which Docker is running which is
+# not the same as the host OS file system. We found that updating a file on the Mac does not trigger fmks while updating
+# the same file inside the running Docker container does trigger even though it is the same file. This means that docker-
+# compose has to be restarted manually on the Mac. This isn't a problem on Linux since Docker doesn't run in a VM.
+
+while [ true ] do
+#
+    find about.md config{e,h}.toml {e,h}content static | entr ./fmks $HUGO_DEST $HUGO_BASEURL
     echo "Sleeping for $HUGO_REFRESH_TIME seconds..."
     sleep $SLEEP
 done
